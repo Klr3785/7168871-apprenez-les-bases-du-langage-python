12fe2d262af5f574d1fd3a283735df8d31d3c44f
Klr3785 <guilloteau.claire@outlook.fr>
2026-03-23 16:02:25 +0100

    Calculatrice.py

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,1 +1,30 @@
-# Ecrivez votre code ici !
+nombre1 = input("Entrer un nombre :")
+nombre2 = input("Entrer un nombre :")
+
+if not nombre1.isnumeric() or not nombre2.isnumeric():
+  print("Les nombres doivent être des nombres entier.")
+raise SystemExit("Fin du programme")
+
+nombre1 = int(nombre1)
+nombre2 = int(nombre2)
+
+operation =  input ("Entrer l'opération souhaitée :") 
+
+if not operation = ["+", "-", "*", "/"]:
+  print("L'opération n'est pas valide.")
+raise SystemExit("Fin du programme")
+
+if operation == "+" :
+  resultat = nombre1 + nombre2
+elif operation == "-":
+  resultat = nombre1 + nombre2
+elif operation == "*":
+  resultat = nombre1 * nombre2
+elif operation == "/":
+  if nombre2=0:
+    print("Il est impossible de diviser un nombre par zéro.")
+    raise SystemExit("Fin du programme") 
+  resultat = round(nombre1 / nombre2)
+  
+print(f "Le résultat est" {resultat} ".")
+
